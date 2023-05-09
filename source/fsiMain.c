8ab5d7f90c1af68c6c41b94b554c15b8bd2d5cd3
Aloke Das <aloke.das@ieee.org>
2023-05-09 18:10:52 +0530

    Basic good machine simulation

--- source/fsiMain.c
+++ source/fsiMain.c
@@ -1,8 +1,16 @@
 #include <vpi_user.h>
+
+typedef struct ReadStimData {
+  FILE *file_ptr; /* test vector file pointer */
+  vpiHandle patin_h; /* pointer to store handle for a Verilog object */
+  vpiHandle patout_h; /* pointer to store handle for a Verilog object */
+} s_ReadStimData, *p_ReadStimData;
+
 PLI_INT32 fsim_compiletf(PLI_BYTE8 *user_data)
 {
   vpiHandle systf_handle, arg_iterator, arg_handle;
   PLI_INT32 arg_type;
+
   /* obtain a handle to the system task instance */
   systf_handle = vpi_handle(vpiSysTfCall, NULL);
   if (systf_handle == NULL)
@@ -29,12 +37,22 @@ PLI_INT32 fsim_compiletf(PLI_BYTE8 *user_data)
     vpi_control(vpiFinish,0); /* abort simulation */
     return(0);
   }
+  /* check the type of object in system task arguments */
+  arg_handle = vpi_scan(arg_iterator);
+  arg_type = vpi_get(vpiType, arg_handle);
+  if (arg_type != vpiNet)
+  {
+    vpi_printf("ERROR: $faultSimulate second arg must be a reg\n");
+    vpi_free_object(arg_iterator); /* free iterator memory */
+    vpi_control(vpiFinish,0); /* abort simulation */
+    return(0);
+  }
   /* check that there are no more system task arguments */
   arg_handle = vpi_scan(arg_iterator);
   arg_type = vpi_get(vpiType, arg_handle);
   if (arg_type != vpiConstant)
   {
-    vpi_printf("ERROR: $faultSimulate second arg must be filename within quotes\n");
+    vpi_printf("ERROR: $faultSimulate third arg must be filename within quotes\n");
     vpi_free_object(arg_iterator); /* free iterator memory */
     vpi_control(vpiFinish,0); /* abort simulation */
     return(0);
@@ -43,7 +61,7 @@ PLI_INT32 fsim_compiletf(PLI_BYTE8 *user_data)
   arg_type = vpi_get(vpiType, arg_handle);
   if (arg_type != vpiConstant)
   {
-    vpi_printf("ERROR: $faultSimulate second arg must be filename within quotes\n");
+    vpi_printf("ERROR: $faultSimulate fourth arg must be filename within quotes\n");
     vpi_free_object(arg_iterator); /* free iterator memory */
     vpi_control(vpiFinish,0); /* abort simulation */
     return(0);
@@ -58,20 +76,78 @@ PLI_INT32 fsim_compiletf(PLI_BYTE8 *user_data)
   }
   return(0);
 }
+PLI_INT32 fsim_simulate_fault(p_cb_data cb_data)
+{
+  char vector[1024]; /* fixed max. size, should use malloc instead */
+  int delay;
+  vpiHandle systf_h, cb_h;
+  s_cb_data data_s;
+  s_vpi_time time_s;
+  s_vpi_value value_s;
+  p_ReadStimData StimData; /* pointer to a ReadStimData structure */
+  /* retrieve system task handle from user_data */
+  systf_h = (vpiHandle)cb_data->user_data;
+  /* get ReadStimData pointer from work area for this task instance */
+  StimData = (p_ReadStimData)vpi_get_userdata(systf_h);
+  value_s.format = vpiBinStrVal;
+  vpi_get_value(StimData->patout_h, &value_s);
+  vpi_printf("net o has the value %s\n", value_s.value.str);
+  /* read next line from the file */
+  char onePat[3];
+  if (fscanf(StimData->file_ptr, "%s\n", onePat) == EOF)
+  {
+    vpi_printf("End-Of-File.\n");
+    fclose(StimData->file_ptr);
+    vpi_control(vpiFinish, 1); /* abort simulation */
+    return(0);
+  }
+  else
+  {
+    value_s.format = vpiBinStrVal;
+    value_s.value.str = onePat;
+    vpi_put_value(StimData->patin_h, &value_s, NULL, vpiNoDelay);
+    vpi_printf("Pat has the value %s\n", value_s.value.str);
+  }
+  /* schedule callback to this routine when time to read next vector */
+  time_s.type = vpiSimTime;
+  time_s.low = 0;
+  time_s.high = 0;
+  data_s.reason = cbReadWriteSynch;
+  data_s.cb_rtn = fsim_simulate_fault;
+  data_s.obj = NULL; /* object required for scaled delays */
+  data_s.time = &time_s;
+  data_s.value = NULL;
+  data_s.user_data = (PLI_BYTE8 *)systf_h;
+  cb_h = vpi_register_cb(&data_s);
+  if (vpi_chk_error(NULL))
+    vpi_printf("An error occurred registering ReadNextStim callback\n");
+  else
+    vpi_free_object(cb_h); /* don’t need callback handle */
+}
 PLI_INT32 fsim_calltf(PLI_BYTE8 *user_data)
 {
-  vpiHandle systf_handle, arg_iterator, arg_handle, reg_handle;
+  vpiHandle cb_h, systf_handle, arg_iterator, arg_handle, reg_handle, net_handle;
   s_vpi_value current_value;
+  s_cb_data cb_data_s;
+  p_ReadStimData StimData; /* pointer to a ReadStimData structure */
+  s_vpi_time time_s;
+
   /* obtain a handle to the system task instance */
   systf_handle = vpi_handle(vpiSysTfCall, NULL);
   /* obtain handle to system task argument
   compiletf has already verified only 3 args with correct type */
+  current_value.format = vpiBinStrVal; /* read value as a string */
   arg_iterator = vpi_iterate(vpiArgument, systf_handle);
   reg_handle = vpi_scan(arg_iterator);
   /* read current value */
-  current_value.format = vpiBinStrVal; /* read value as a string */
   vpi_get_value(reg_handle, &current_value);
   vpi_printf("Signal %s ", vpi_get_str(vpiFullName, reg_handle));
+  vpi_printf("has the value %s\n", current_value.value.str);
+
+  net_handle = vpi_scan(arg_iterator);
+  /* read current value */
+  vpi_get_value(net_handle, &current_value);
+  vpi_printf("Signal %s ", vpi_get_str(vpiFullName, net_handle));
   vpi_printf("has the value %s\n", current_value.value.str);
 
   arg_handle = vpi_scan(arg_iterator);
@@ -87,18 +163,15 @@ PLI_INT32 fsim_calltf(PLI_BYTE8 *user_data)
     return(0);
   }
   char onePat[3];
-  if (fscanf(patFile, "%s\n", onePat))
+  if (fscanf(patFile, "%s\n", onePat) != EOF)
   {
   current_value.format = vpiBinStrVal;
   current_value.value.str = onePat;
   vpi_put_value(reg_handle, &current_value, NULL, vpiNoDelay);
-  vpi_printf("has the value %s\n", current_value.value.str);
-  }
-  else
-  {
-    vpi_printf("ERROR: $faultSimulate could not read pattern \n");
-    vpi_control(vpiFinish, 1); /* abort simulation */
-    return(0);
+  vpi_get_value(net_handle, &current_value);
+  vpi_printf("net o has the value %s\n", current_value.value.str);
+  vpi_get_value(reg_handle, &current_value);
+  vpi_printf("Pat has the value %s\n", current_value.value.str);
   }
 
   arg_handle = vpi_scan(arg_iterator);
@@ -107,6 +180,23 @@ PLI_INT32 fsim_calltf(PLI_BYTE8 *user_data)
   vpi_printf("Fault file %s \n", current_value.value.str);
 
   vpi_free_object(arg_iterator); /* free iterator memory */
+
+  /* setup a callback for start of simulation */
+  time_s.type = vpiSimTime;
+  time_s.low = 0;
+  time_s.high = 0;
+  cb_data_s.reason = cbReadWriteSynch;
+  cb_data_s.cb_rtn = fsim_simulate_fault;
+  cb_data_s.obj = NULL;
+  cb_data_s.time = &time_s;
+  cb_data_s.user_data = (PLI_BYTE8 *)systf_handle; /* pass systf_h */
+  StimData = (p_ReadStimData)malloc(sizeof(s_ReadStimData));
+  StimData->file_ptr = patFile;
+  StimData->patin_h = reg_handle;
+  StimData->patout_h = net_handle;
+  vpi_put_userdata(systf_handle, (PLI_BYTE8 *)StimData);
+  cb_h = vpi_register_cb(&cb_data_s);
+  vpi_free_object(cb_h); /* don’t need callback handle */
   return(0);
 }
 void fsim_register()
